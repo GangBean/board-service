a012a979ada3a9573e6354782e3e9ad4964e3175
Gangbean <xbox2537@gmail.com>
2023-06-10 15:06:25 +0900

    (feat) implements google login

--- src/main/java/com/gangbean/springboot/web/IndexController.java
+++ src/main/java/com/gangbean/springboot/web/IndexController.java
@@ -1,6 +1,9 @@
 package com.gangbean.springboot.web;
 
+import com.gangbean.springboot.config.auth.LoginUser;
+import com.gangbean.springboot.config.auth.dto.SessionUser;
 import com.gangbean.springboot.service.posts.PostsService;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
